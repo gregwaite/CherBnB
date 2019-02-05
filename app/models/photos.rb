9c238279e99d15a0c3c6615c62ee487ec1d111f5
App Academy Student <aa@Apps-Mac-mini.local>
2019-02-05 16:55:33 -0500

    Start AWS

---
+++ app/models/photos.rb
@@ -0,0 +1,5 @@
+class Photos < ApplicationRecord
+
+
+
+end
--- app/models/spot.rb
+++ app/models/spot.rb
@@ -26,4 +26,6 @@ class Spot < ApplicationRecord
   belongs_to :user,
     class_name: "User",
     foreign_key: :owner_id
+
+  has_one_attached :photo
 end

2339396a84bbc1a567da7e222d9a2f658a39182b
Greg Waite <waite68@gmail.com>
2019-02-27 16:55:22 -0500

    Try to fix migrations

--- db/migrate/20190226154445_add_guests_column_to_spots2_electric_boogaloo.rb
+++ db/migrate/20190226154445_add_guests_column_to_spots2_electric_boogaloo.rb
@@ -1,5 +1,4 @@
 class AddGuestsColumnToSpots2ElectricBoogaloo < ActiveRecord::Migration[5.2]
   def change
-    add_column :spots, :max_guests, :integer
   end
 end
--- db/migrate/20190226154457_add_guests_column_to_bookings2_electric_boogaloo.rb
+++ db/migrate/20190226154457_add_guests_column_to_bookings2_electric_boogaloo.rb
@@ -1,5 +1,4 @@
 class AddGuestsColumnToBookings2ElectricBoogaloo < ActiveRecord::Migration[5.2]
   def change
-    add_column :bookings, :num_guests, :integer
   end
 end
